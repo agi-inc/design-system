4e10c4d58e0bec0ba354999032818e821c57b5db
Dharan Ganesan <14145706+dhrn@users.noreply.github.com>
2024-08-20 12:01:12 +0530

    Fix zoom on mobile devices (#5)

--- libs/ui/src/lib/ui/input.tsx
+++ libs/ui/src/lib/ui/input.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@multion/ui-util';
 
 const inputVariants = cva(
-  'flex w-full rounded-xs border border-input bg-background px-4 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none focus-visible:shadow',
+  'flex w-full rounded-xs border border-input bg-background px-4 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none focus-visible:shadow text-base',
   {
     variants: {
       variantSize: {
